8388b97943f36054079d17103e28b3d15d35a13e
Kaichunyu <kaichun.yu@griffithuni.edu.au>
2024-03-13 13:20:13 +1000

    Stlying update

--- To-Do-List/App.js
+++ To-Do-List/App.js
@@ -6,24 +6,27 @@ export default function App() {
     pressed ? [styles.pressed, styles.button] : [styles.button];
   return (
     <View style={styles.container}>
-      <View style={[styles.title, {flex: 1}]}>
+      <View style={styles.topboard}>
         <Text style={styles.title}>My Todo List</Text>
       </View>
 
-      <View style={{flex: 14, gap: 10, padding: 10}}>
-        <View style={styles.list}><Text style={styles.items}>Buy milk</Text></View>
-        <View style={styles.list}><Text style={styles.items}>Buy eggs</Text></View>
-        <View style={styles.list}><Text style={styles.items}>Buy veges</Text></View>
-        <View style={styles.list}><Text style={styles.items}>Buy mango</Text></View>
+
+
+      <View style={styles.middleboard}>
+        <View style={styles.list}><Text style={styles.item}>Buy milk</Text></View>
+        <View style={styles.list}><Text style={styles.item}>Buy eggs</Text></View>
+        <View style={styles.list}><Text style={styles.item}>Buy veges</Text></View>
+        <View style={styles.list}><Text style={styles.item}>Buy mango</Text></View>
       </View>
 
-      <View style={[styles.button, {flex: 1, borderTopWidth: 1}]}>
+
+
+      <View style={styles.bottomboard}>
         <Pressable
           style={getButStyle}
           onPress={() => {
             console.log('pressed')
           }}
-        
         >
           <Text style={styles.button}>Add New Todo</Text>
         </Pressable>
@@ -36,36 +39,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    // alignItems: 'center',
-    // justifyContent: 'center',
     flexDirection: 'column',
     marginTop: 50,
     marginBottom: 50,
     marginHorizontal: 10,
   },
 
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  topboard: {
+    flex: 1,
+    borderBottomWidth: 2,
     alignItems: 'center',
     justifyContent: 'center',
-    borderBottomWidth: 2,
+  },
+
+  middleboard: {
+    flex: 14, 
+    gap: 10, 
+    padding: 10,
 
   },
 
+  bottomboard: {
+    flex: 1,
+    borderTopWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
   list: {
-    backgroundColor: 'lightblue',
+    backgroundColor: 'lightgreen',
     borderRadius: 10,
     padding: 10,
   },
 
-  items: {
+  item: {
     fontSize: 15,
   },
 
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
     fontSize: 20,
     fontWeight: '400',
     color: '#2196F3',
